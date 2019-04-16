3ec00afc96702dcad045f6ee76ddff58fc6df238
Yarekr <jaroslaw.rajchert@gmail.com>
2019-04-16 18:00:15 +0200

    Data sql - updates

--- carrental/src/main/resources/data.sql
+++ carrental/src/main/resources/data.sql
@@ -2,7 +2,7 @@ insert into car values(1,'Lada', 'SCRAP', 0, 500000);
 insert into car values(2,'Volvo', 'OK', 0, 50000);
 insert into car values(3,'Toyota', 'CAR_REVIEW', 0, 150000);
 insert into car values(4,'Renault', 'OK', 1, 10000000);
+insert into car values(5,'Mazda', 'OK', 0, 20000);
 
-insert into users
-values (1,'Admin','DIAGNOSE,BOOK_CAR');
-values (2,'Tomasz','BOOK_CAR');
+insert into users values (1,'Admin','DIAGNOSE,BOOK_CAR');
+insert into users values(2,'Tomasz','BOOK_CAR');
